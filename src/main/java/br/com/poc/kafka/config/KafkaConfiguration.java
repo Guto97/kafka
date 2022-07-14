d9cbc83d87ba9f3b3b47e20739e210aca1b9c595
Gustavo Fabiano <gustavo.fabiano68@gmail.com>
2022-07-14 14:56:22 -0300

    first commit

---
+++ src/main/java/br/com/poc/kafka/config/KafkaConfiguration.java
@@ -0,0 +1,21 @@
+package br.com.poc.kafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConfiguration {
+
+    private String topic;
+
+    public KafkaConfiguration(@Value("${kafka.name}") String topic) {
+        this.topic = topic;
+    }
+
+    @Bean
+    public NewTopic create() {
+        return new NewTopic(topic, 3, (short) 1);
+    }
+}
