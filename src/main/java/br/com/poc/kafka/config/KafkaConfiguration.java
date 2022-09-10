f1c90b71e1ec10b3a744aac7bf47aff31bf2eac6
Renato <rena.f.oliveira@gmail.com>
2022-09-10 14:08:20 -0300

    update class KafkaConfiguration - add retention property

--- src/main/java/br/com/poc/kafka/config/KafkaConfiguration.java
+++ src/main/java/br/com/poc/kafka/config/KafkaConfiguration.java
@@ -1,21 +1,31 @@
 package br.com.poc.kafka.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaConfiguration {
 
     private String topic;
 
+    @Value("${spring.kafka.producer.log.retention.minutes}")
+    private String retention_in_minutes;
+
     public KafkaConfiguration(@Value("${kafka.name}") String topic) {
         this.topic = topic;
     }
 
     @Bean
-    public NewTopic create() {
-        return new NewTopic(topic, 3, (short) 1);
+    public NewTopic myTopic() {
+        return TopicBuilder.name(topic)
+                .partitions(3)
+                .replicas(1)
+                .config(TopicConfig.RETENTION_MS_CONFIG, retention_in_minutes)
+                .build();
     }
+
 }
